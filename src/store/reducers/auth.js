875278fc6695371cc6a5a44e0654cb097cb618e7
lozanovsvetoslav <lozanovsvetoslav@gmail.com>
2021-04-09 11:37:08 +0300

    improvements and lazy loading

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -5,7 +5,8 @@ const initialState = {
     token: null,
     userId: null,
     error: null,
-    loading: false
+    loading: false,
+    authRedirectPath: '/'
 }
 
 const reducer = (state = initialState, action) => {
@@ -25,7 +26,9 @@ const reducer = (state = initialState, action) => {
                 loading: false
             });
         case actionTypes.AUTH_LOGOUT:
-            return updateObject(state, {toke: null, userId: null});
+            return updateObject(state, {token: null, userId: null});
+        case actionTypes.SET_AUTH_REDIRECT_PATH:
+            return updateObject(state, {authRedirectPath: action.path});
         default:
             return state;
     } 
